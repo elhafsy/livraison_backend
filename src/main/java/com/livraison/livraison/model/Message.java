1d8337b3dad8c4da6448724e7ac5b4bdd5fb131e
fayssaldd <fayssaldd982@gmail.com>
2025-04-13 23:36:21 +0100

    feat:(chat) add Message Entity

---
+++ src/main/java/com/livraison/livraison/model/Message.java
@@ -0,0 +1,46 @@
+package com.livraison.livraison.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "sent_at")
+    private LocalDateTime sentAt;
+
+    @Column(name = "is_read")
+    private boolean isRead;
+
+    @Column(name = "read_at")
+    private LocalDateTime readAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.sentAt = LocalDateTime.now();
+        this.isRead = false;
+    }
+
+}
